b9d9ea0141dc00373d5b8e2bff05e05ea51f643e
Lkanath Panda <lokanthpanda128@gmai.com>
2023-10-27 21:11:46 +0530

    Navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,62 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "./Image/New Logo.png";
+
+import "./css/Navbar.css"
 const Navbar = () => {
   return (
     <>
-   <header>
-    <div>LOGO</div>
-    <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/dashboard">Dashboard</NavLink></li>
-        <li><NavLink to="/nft">NFT</NavLink></li>
-        <li><NavLink to="/earn">Earn</NavLink></li>
-        <li><NavLink to="/buy">Buy</NavLink></li>
-        <li><NavLink to="/referals">Referals</NavLink></li>
-        <li><NavLink to="/ecosystem">Ecosystem</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-    </ul>
-   </header>
-    </>
-  )
-}
 
-export default Navbar
+
+      <div className="nav">
+        <div className="nav-container">
+          <div className="nav-left">
+            <div className="logo">
+              <div className="logo-image">
+                <img src={Logo} alt="the logo of Romi.Finance" />
+              </div>
+              <div className="logo-brand">Romi.Finance</div>
+            </div>
+          </div>
+
+          <div className="nav-mid">
+            <ul>
+              <li>
+                <NavLink className="nav-items" to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink className="nav-items" to="/dashboard">Dashboard</NavLink>
+              </li>
+              <li>
+                <NavLink className="nav-items" to="/nft">NFT</NavLink>
+              </li>
+              <li>
+                <NavLink className="nav-items" to="/earn">Earn</NavLink>
+              </li>
+              <li>
+                <NavLink className="nav-items" to="/buy">Buy</NavLink>
+              </li>
+              <li>
+                <NavLink className="nav-items" to="/referals">Referals</NavLink>
+              </li>
+              <li>
+                <NavLink className="nav-items" to="/ecosystem">Ecosystem</NavLink>
+              </li>
+              <li>
+                <NavLink className="nav-items" to="/about">About</NavLink>
+              </li>
+            </ul>
+          </div>
+
+          <div className="nav-right">
+            <div className="trade"><button>Trade</button></div>
+            <div className="wallet"><button>Connect Wallet</button></div>
+            <div className="networks"><button>N|:</button></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
