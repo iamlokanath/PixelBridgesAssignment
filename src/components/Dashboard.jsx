b4e1e9ec53e81c1ac2708a14017129992980efb1
Lkanath Panda <lokanthpanda128@gmai.com>
2023-10-30 00:36:33 +0530

    Earn Page Created

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,119 @@
 import React from 'react'
 import Navbar from './Navbar'
-
+import Stats from './Image/ic_avalanche_24.4f4088f4.svg'
+import "./css/Dashboard.css"
 const Dashboard = () => {
   return (
     <>
     <Navbar/>
-    <div>
-      Dashboard page Comming Soon....
+
+    <div className="dashboard">
+      <div className="dashboard-container">
+
+        {/* stats starts here */}
+        <div className="stats">
+          <div className="stats-container">
+            <div className="stats-heading">
+                <div className="stats-head">
+                  <div className="stats-title">Stats</div>
+                  <div className="stats-logo"><img src={Stats} alt="Stats" /></div>
+                </div>
+                <div className="stats-para">
+                Ethereum Total Stats start from 06 Jan 2022. <br />
+                For detailed stats:
+                </div>
+            </div>
+            <div className="stats-card">
+                <div className="stats-card-container">
+
+                  {/* card1 starts */}
+                  <div className="stats-card-1">
+                    <div className="stats-card-1-container">
+                      <div className="stats-card1-title">Overview</div>
+                      <div className="stats-menu">
+
+                        <div className="stats-menu-item">
+                          <div className="stats-menu-item-left">AUM</div>
+                          <div className="stats-menu-item-right">$34</div>
+                        </div>
+
+                        <div className="stats-menu-item">
+                          <div className="stats-menu-item-left">$ZLP Pool</div>
+                          <div className="stats-menu-item-right">$34</div>
+                        </div>
+
+                        <div className="stats-menu-item">
+                          <div className="stats-menu-item-left">24h Volume</div>
+                          <div className="stats-menu-item-right">$0</div>
+                        </div>
+
+                        <div className="stats-menu-item">
+                          <div className="stats-menu-item-left">Long Positions</div>
+                          <div className="stats-menu-item-right">$0</div>
+                        </div>
+
+                        <div className="stats-menu-item">
+                          <div className="stats-menu-item-left">Short Positions</div>
+                          <div className="stats-menu-item-right">$0</div>
+                        </div>
+
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* card1 starts */}
+
+                  {/* card1 starts */}
+                  <div className="stats-card-1">
+                    <div className="stats-card-1-container">
+                      <div className="stats-card1-title">Overview</div>
+                      <div className="stats-menu">
+
+                        <div className="stats-menu-item">
+                          <div className="stats-menu-item-left">AUM</div>
+                          <div className="stats-menu-item-right">$34</div>
+                        </div>
+
+                        <div className="stats-menu-item">
+                          <div className="stats-menu-item-left">$ZLP Pool</div>
+                          <div className="stats-menu-item-right">$34</div>
+                        </div>
+
+                        <div className="stats-menu-item">
+                          <div className="stats-menu-item-left">24h Volume</div>
+                          <div className="stats-menu-item-right">$0</div>
+                        </div>
+
+                        <div className="stats-menu-item">
+                          <div className="stats-menu-item-left">Long Positions</div>
+                          <div className="stats-menu-item-right">$0</div>
+                        </div>
+
+                        <div className="stats-menu-item">
+                          <div className="stats-menu-item-left">Short Positions</div>
+                          <div className="stats-menu-item-right">$0</div>
+                        </div>
+                  {/* card1 starts */}
+
+                  
+                      </div>
+                    </div>
+                  </div>
+
+
+
+                </div>
+            </div>
+          </div>
+        </div>
+
+        {/* tokens starts here */}
+        <div className="tokens">
+          <div className="tokens-container">
+
+          </div>
+        </div>
+      </div>
     </div>
     </>
   )
--- src/components/Earn.jsx
+++ src/components/Earn.jsx
@@ -1,15 +1,153 @@
-import React from 'react'
-import Navbar from './Navbar'
-
+import React from "react";
+import Navbar from "./Navbar";
+import "./css/Earn.css";
+import Footer from "./Footer";
 const Earn = () => {
   return (
     <>
-    <Navbar/>
-    <div>
-      Earn page Comming Soon....
-    </div>
-    </>
-  )
-}
+      <Navbar />
+      <div className="earnpage">
+        <div className="earn-container">
+          {/* earn starts here */}
 
-export default Earn
+          <div className="earn-heading">
+            <h2>Earn</h2>
+            <p>Stake $ZOMI and $ZLP to earn rewards.</p>
+          </div>
+          <div className="earn">
+            <div className="earn-left">
+              <div className="earn-left-first">
+                I am Earn component left first
+              </div>
+              <div className="earn-left-second">
+                I am Earn component left second
+              </div>
+            </div>
+            <div className="earn-right">
+              <div className="earn-right-first">
+                I am earn right component first
+              </div>
+              <div className="earn-right-second">
+                I am earn right component second
+              </div>
+            </div>
+          </div>
+
+          {/* earn ends here */}
+
+          {/* vest starts here */}
+          <div className="vest-heading">
+            <h2>Vest</h2>
+            <p>
+              Convert esZOMI tokens to $ZOMI tokens. <br />
+              Please read the vesting details before using the vaults.
+            </p>
+          </div>
+          <div className="vest">
+            <div className="vest-left">
+              <div className="vest-left-container">
+                
+                <div className="vest-left-first">
+                $ZOMI Vault
+                </div>
+                <div className="vest-left-second">
+                  <div className="vast-left-second-menu">
+                    <div className="vast-left-second-items">
+                    <div className="staked-tokens-left">
+                    Staked Tokens
+                    </div>
+                    <div className="staked-tokens-right">
+                    ...
+                    </div>
+                    </div>
+                    <div className="vast-left-second-items">
+                    <div className="staked-tokens-left">
+                    Staked Tokens
+                    </div>
+                    <div className="staked-tokens-right">
+                    0.00 / ...
+                    </div>
+                    </div>
+                    <div className="vast-left-second-items">
+                    <div className="staked-tokens-left">
+                    Staked Tokens
+                    </div>
+                    <div className="staked-tokens-right">
+                    0.0000 / 0.0000
+                    </div>
+                    </div>
+                    <div className="vast-left-second-items">
+                    <div className="staked-tokens-left">
+                    Staked Tokens
+                    </div>
+                    <div className="staked-tokens-right">
+                    0.0000 $ZOMI
+                    </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="vest-left-third">
+                 <button>Connect Wallet</button>
+                </div>
+              </div>
+            </div>
+
+
+            <div className="vest-left">
+              <div className="vest-left-container">
+                
+                <div className="vest-left-first">
+                $ZOMI Vault
+                </div>
+                <div className="vest-left-second">
+                  <div className="vast-left-second-menu">
+                    <div className="vast-left-second-items">
+                    <div className="staked-tokens-left">
+                    Staked Tokens
+                    </div>
+                    <div className="staked-tokens-right">
+                    ...
+                    </div>
+                    </div>
+                    <div className="vast-left-second-items">
+                    <div className="staked-tokens-left">
+                    Staked Tokens
+                    </div>
+                    <div className="staked-tokens-right">
+                    0.00 / ...
+                    </div>
+                    </div>
+                    <div className="vast-left-second-items">
+                    <div className="staked-tokens-left">
+                    Staked Tokens
+                    </div>
+                    <div className="staked-tokens-right">
+                    0.0000 / 0.0000
+                    </div>
+                    </div>
+                    <div className="vast-left-second-items">
+                    <div className="staked-tokens-left">
+                    Staked Tokens
+                    </div>
+                    <div className="staked-tokens-right">
+                    0.0000 $ZOMI
+                    </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="vest-left-third">
+                 <button>Connect Wallet</button>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+    <Footer/>
+    </>
+  );
+};
+
+export default Earn;
